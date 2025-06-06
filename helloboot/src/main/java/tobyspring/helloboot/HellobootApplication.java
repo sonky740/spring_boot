b0c071eb44edfcfb551e1d8e9c4cc0c13cc981ab
Sonky <sonky740@gmail.com>
2025-06-06 21:26:15 +0900

    feat: add servlet

--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,13 +1,36 @@
 package tobyspring.helloboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 public class HellobootApplication {
+  public static void main(String[] args) {
+    ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+    WebServer webServer = serverFactory.getWebServer(servletContext -> {
+      servletContext.addServlet("hello", new HttpServlet() {
+        @Override
+        protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+          String name = req.getParameter("name");
+          if (name == null || name.isEmpty()) {
+            name = "World";
+          }
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellobootApplication.class, args);
-	}
-
+          resp.setStatus(HttpStatus.OK.value());
+          resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+          resp.getWriter().println("Hello " + name);
+        }
+      }).addMapping("/hello");
+    });
+    webServer.start();
+  }
 }
